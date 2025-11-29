1af82e7240bfd2580f3ec2ba8536e40e84d7b5e5
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-11-29 20:09:59 +0100

    fix

--- bash/new_site.sh
+++ bash/new_site.sh
@@ -20,12 +20,7 @@ sudo sed -i "s/DOMAIN/${domain}/g" /etc/nginx/sites-available/$domain
 sudo sed -i "s/PORT/${port}/g" /etc/nginx/sites-available/$domain
 sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 sudo nginx -t && sudo systemctl restart nginx
-sudo sed -i "/apps : \[/r $HOME/ultra/templates/ecobloc.txt" $HOME/ecosystem.config.js
-sudo sed -i "s/name : \"\",/name : \"${gitname}\",/g" $HOME/ecosystem.config.js
-sudo sed -i "s/cwd : \"\",/cwd : \"${gitname}\",/g" $HOME/ecosystem.config.js
 cd $HOME && git clone $giturl
 cd $HOME/$gitname && tmux &&
-cd $HOME && pm2 delete all
-cd $HOME && pm2 start ecosystem.config.js
-pm2 save
-cd $HOME/ultra && echo && npm run
+pm2 start npm --name "$gitname" -- start && pm2 save
+cd $HOME/ultra && clear && pm2 ls && npm run
