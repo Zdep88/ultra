f68c412315650240e7af92a9dd271cac686d057e
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-12-04 23:08:36 +0100

    follow up

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,4 @@ source ./.env
 set +a
 sudo -i -u postgres psql -c "CREATE ROLE \"$DATABASE_USER\" WITH LOGIN PASSWORD '$DATABASE_PASSWORD'"
 sudo -i -u postgres psql -c "CREATE DATABASE \"$DATABASE_NAME\" OWNER \"$DATABASE_USER\""
+cd $HOME/ultra && npm run reset
